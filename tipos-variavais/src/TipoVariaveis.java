d942800002ace8201a7143296cdf1c7638b09413
MrFilipeOliveira <mr.filipe@msn.com>
2023-04-14 14:38:15 +0100

    Tipos de veriaveis

--- tipos-variavais/src/TipoVariaveis.java
+++ tipos-variavais/src/TipoVariaveis.java
@@ -1,5 +1,37 @@
+
+/* */
+
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        double salarioMinimo = 2500.33;
+        byte idade = 123;
+        short ano = 2023;
+        int cep = 12345678;
+        long cpf = 12345678901L;
+        float pi = 3.14f;
+        double salario = 1234.50;
+        
+        short numeroCurto = 1;
+        short numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;  
+        
+        /*⛔️VALOR_DE_PI = 10.4;*/
+
+
+        System.out.println(salarioMinimo);
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cep);
+        System.out.println(cpf);
+        System.out.println(pi);
+        System.out.println(salario);
+        System.out.println(numeroCurto);
+        System.out.println(numeroCurto2);
+        System.out.println(VALOR_DE_PI);
+
     }
 }
